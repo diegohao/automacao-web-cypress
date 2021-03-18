d3de5b78758457039c6726af221b701a00bba3c1
Diego Henrique <58568683+diegohao@users.noreply.github.com>
2021-03-18 12:25:36 -0300

    Criação do arquivo locators.js

---
+++ cypress/support/locators.js
@@ -0,0 +1,18 @@
+const locators = {
+  MENU: {
+    FORMULARIO: {
+      CRIAR_USUARIOS: '.active > .collapsible-body > ul > :nth-child(1) > a'
+    }
+  },
+  CRIAR_USUARIOS: {
+    NOME: '#user_name',
+    ULTIMO_NOME: '#user_lastname',
+    EMAIL: '#user_email',
+    ENDERECO: '#user_address',
+    UNIVERSIDADE: '#user_university',
+    PROFISSAO: '#user_profile',
+    GENERO: '#user_gender',
+    IDADE: '#user_age',
+    BTN_CRIAR: '.actions > input'
+  }
+}
