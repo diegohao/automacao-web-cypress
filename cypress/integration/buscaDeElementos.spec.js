7d9c941e161457a1143454fd5292904383540b95
Diego Henrique <58568683+diegohao@users.noreply.github.com>
2021-03-22 11:33:06 -0300

    Alteração no beforeEach
    O MENU.BUSCA_DE_ELEMENTOS.LINKS foi removido do beforeEach e adicionado em cada cenário.

--- cypress/integration/buscaDeElementos.spec.js
+++ cypress/integration/buscaDeElementos.spec.js
@@ -6,25 +6,28 @@ describe('Teste funcional na busca de elementos', () => {
     beforeEach(() => {
         cy.visit('/')
         cy.contains('Busca de elementos').click()
-        cy.get(loc.MENU.BUSCA_DE_ELEMENTOS.LINKS).click()
     })
 
     it('Link: Ok 200 - Success', () => {
+        cy.get(loc.MENU.BUSCA_DE_ELEMENTOS.LINKS).click()
         cy.get(loc.LINKS.SUCCESS).click()
         cy.get(loc.LINKS.MENSAGEM).should('contain', 'Success')        
     })
 
     it('Link: Erro 400 - Bad Request', () => {
+        cy.get(loc.MENU.BUSCA_DE_ELEMENTOS.LINKS).click()
         cy.get(loc.LINKS.BAD_REQUEST).click()
         cy.get(loc.LINKS.MENSAGEM).should('contain', 'Bad Request')
     })
 
     it('Link: Erro 404 - Page not found', () => {
+        cy.get(loc.MENU.BUSCA_DE_ELEMENTOS.LINKS).click()
         cy.get(loc.LINKS.PAGE_NOT_FOUND).click()
         cy.get(loc.LINKS.MENSAGEM).should('contain', 'Page Not Found')        
     })
 
     it('Link: Erro 500 - Internal Server Error', () => {
+        cy.get(loc.MENU.BUSCA_DE_ELEMENTOS.LINKS).click()
         cy.get(loc.LINKS.INTERNAL_SERVER_ERROR).click()
         cy.get(loc.LINKS.MENSAGEM).should('contain', 'Server Error')
     })
