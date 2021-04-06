8a0c205ad3d607e29542ae8d1eee83530816bc13
Diego Henrique <58568683+diegohao@users.noreply.github.com>
2021-04-06 18:01:41 -0300

    Inclusão do cenário de validar alert

--- cypress/integration/mudancaDeFoco.spec.js
+++ cypress/integration/mudancaDeFoco.spec.js
@@ -1,3 +1,17 @@
 /// <reference types="cypress" />
 
-//TODO iniciar implementação
+import loc from '../support/locators'
+
+describe('Teste funcional na busca de elementos', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.xpath('/html/body/div[2]/div[1]/ul/li[3]/a').click()
+    })
+  
+  it('Validar alert', () => {
+    cy.xpath('/html/body/div[2]/div[2]/div[2]/div/div[1]/ul/li[1]/button').click()
+    cy.on('window:alert', msg => {
+      expect(msg).to.be.equal('Eu sou um alerta JS!')
+    })  
+  })
+}
